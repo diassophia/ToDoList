dda771783ebb44aab5e45b19475f4d86c8b11a9b
diassophia <sophia.l.dias@aluno.senai.br>
2026-03-26 13:53:04 -0300

    feat: estrutura para tabelas e migrations

---
+++ Models/Entities/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace ToDoList.Models.Entities
+{
+    public class Usuario
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+        public ICollection<Tarefa> Tarefas { get; set; } = new List<Tarefa>();
+
+
+    }
+}
